dbb89e52f8f9ba531f77212237caa3883fd833af
Fanny <fanny.mouly@epsi.fr>
2020-05-26 17:47:39 +0200

    partie suite

--- webpack.common.js
+++ webpack.common.js
@@ -4,12 +4,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    contentBase: "./dist",
+  },
+
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
