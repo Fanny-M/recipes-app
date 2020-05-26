8b30acb071d4a8f79085a3df9e48ed97237a26d4
Fanny <fanny.mouly@epsi.fr>
2020-05-26 17:09:42 +0200

    suite avancement

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
+const htmlWebpackTemplate = require("html-webpack-template");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -9,9 +11,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: "application_recette",
       meta: { viewport: "width=device-width, initial-scale=1.0" },
+      inject: true,
+      template: htmlWebpackTemplate,
+      link: [
+        "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap",
+      ],
     }),
   ],
   module: {
